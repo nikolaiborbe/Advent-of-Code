f67a606d161914e59142d2a564c53f15059b9808
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-12-03 18:01:23 +0100

    update day 2, better solution

--- src/2024/day-2/day2.py
+++ src/2024/day-2/day2.py
@@ -1,76 +1,27 @@
-file = [x.strip() for x in open("test")]
+file = [x.strip() for x in open("input")]
 
-def part1():
-    new_file = []
-    for i in file:
-        new_file.append([int(x) for x in i.split()])
+def check(l):
+    f1 = l[1] - l[0]
+    for i in range(len(l) - 1):
+        dif = l[i + 1] - l[i]
+        if f1 * dif <= 0 or abs(dif) < 1 or abs(dif) > 3:
+            return False
+    return True
+
+
+def check2(l):
+    if check(l):
+        return True
+    for i in range(len(l)):
+        l2 = l[:i] + l[i + 1:]
+        if check(l2):
+            return True
+    return False
     
-    good = []
-    for i in new_file:
-        top_sort = sorted(i)
-        bot_sort = sorted(i, reverse=True)
-        if i == top_sort:
-            good.append(i)
-        elif i == bot_sort:
-            good.append(i)
-        
-    good_list = 0
-    for i in good:
-        safe = True
-        for j in range(1, len(i)):
-            dif = abs(i[j-1] - i[j])
-            if abs(i[j-1] - i[j]) > 3 or abs(i[j-1] - i[j]) == 0:
-                safe = False
-            else:
-                pass
-        if safe:
-            good_list += 1
-    return good_list
-                
-def part2():
-    new_file = []
-    for i in file:
-        new_file.append([int(x) for x in i.split()])
-
-    for i in range(len(new_file)-1, -1, -1):
-        for j in range(1, len(new_file[i])):
-            if abs(new_file[i][j] - new_file[i][j-1]) > 3:
-                new_file.pop(i)
-            else:
-                pass
-
-    count = 0
-    for i in range(len(new_file)-1, -1, -1):
-        if new_file[i][0] < new_file[i][-1]:
-            incr = True
-        else:
-            incr = False
-
-        index = False
-        for j in range(1, len(new_file[i])):
-            if incr:
-                if new_file[i][j-1] >= new_file[i][j]:
-                    index = j
-                    break
-            if not incr:
-                if new_file[i][j-1] <= new_file[i][j]:
-                    index = j
-                    break
-        if index != False:
-            holder = new_file[i]
-            holder.pop(index)
-            if holder == sorted(holder):
-                count += 1
-    print(count)
-            
-        
-
-
-
     
-        
-
-part2()
-            
-
-        
+    
+    
+reports = [list(map(int, line.split())) for line in file]
+part1 = sum(check(x) for x in reports)
+part2 = sum(check2(x) for x in reports)
+print(part2)
