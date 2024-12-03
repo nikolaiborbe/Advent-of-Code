b4994a7d4aa8ca183d1f70d7678e4ae655d78502
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-12-03 18:51:40 +0100

    add day 3

--- src/2024/day-3/day3.py
+++ src/2024/day-3/day3.py
@@ -1,3 +1,28 @@
 import re 
 
-mul\((\d{1,3}),(\d{1,3})\)
+file = open("input").read()
+
+def p1():
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    matches = [list(map(int, (x, y))) for x, y in re.findall(pattern, file)]
+    print(sum(x * y for x, y in matches))
+
+def p2():
+    do = True
+    pattern = r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"
+    matches = re.findall(pattern, file)
+    new = []
+    for i in matches:
+        if i[0] == "do()":
+            do = True
+            continue
+        elif i[0] == "don't()":
+            do = False
+            continue
+        if do:
+            new.append(int(i[1]) * int(i[2]))
+
+    print(sum(new))
+
+p1()
+p2() 
