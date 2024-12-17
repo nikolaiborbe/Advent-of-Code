89d6875ad989eaafc14c3b04159d4a905bc119e8
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-12-17 16:12:11 +0100

    .

---
+++ src/2024/day8/day8.py
@@ -0,0 +1,16 @@
+
+import sys
+sys.path.append('/Users/nikolaiborbe/Documents/Programming/Advent-of-Code/src/')
+from aoc_utils import *
+
+filename = "test"
+#lines = [x.strip() for x in open(filename)]
+#file = open(filename).read()
+
+def p1():
+    pass
+
+def p2():
+    pass
+
+print_(p1()) # print and copy answer to clipboard
